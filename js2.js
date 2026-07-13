8f23dc3858d330826cd8a65128d94d527a60444c
Paulin <shemampa2010@gmail.com>
2026-07-13 15:52:30 +0200

    Practice DOM textContent and querySelectorAll

---
+++ js2.js
@@ -0,0 +1,11 @@
+function modifier() {
+
+    let paragraphes = document.querySelectorAll("p");
+
+    paragraphes[0].textContent = "Nom : Pablo";
+
+    paragraphes[1].textContent = "Age = 16";
+    
+    paragraphes[2].textContent = "Pays = Rwanda";
+
+}
