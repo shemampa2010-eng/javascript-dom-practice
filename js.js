3ac46c42a9c60ba8762500e552e647888949fc10
Paulin <shemampa2010@gmail.com>
2026-07-13 15:51:14 +0200

    utilisation de DOM

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
-let jsp = document.querySelectorAll('p')
-jsp.forEach(function (p){
-    p.innerHTML = "Bonjour";
-});
+function modifier() {
+
+    document.querySelector("h1").textContent = "Mon profil JavaScript";
+
+    let paragraphes = document.querySelectorAll("p");
+
+    paragraphes[0].textContent = "Nom : Pablo";
+
+    paragraphes[1].textContent = "Age : 16";
+
+}
