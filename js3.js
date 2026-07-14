cc621850993bfec6f17deff29a7c84642e3423b5
Paulin <shemampa2010@gmail.com>
2026-07-14 11:44:17 +0200

    adjusted the js file

--- js3.js
+++ js3.js
@@ -1,3 +1,7 @@
 function change(){
     document.querySelector('h1').innerHTML = "Profil de Pablo"
+    let p = document.querySelectorAll('p')
+    p[0].textContent = "Nom: Mugabo"
+    p[1].textContent = "Age: 16ans"
+    p[2].textContent = "Pays: Rwanda"
 }
