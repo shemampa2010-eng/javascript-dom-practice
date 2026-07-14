a6deac0d9160a7ebb433e797056746db18b50831
Paulin <shemampa2010@gmail.com>
2026-07-14 12:30:41 +0200

    used toggle

--- js3.js
+++ js3.js
@@ -9,7 +9,7 @@ function important(){
     let titre = document.querySelector('h1')
     let paragraphes = document.querySelectorAll('p')
     paragraphes.forEach(function(p){
-        p.classList.add("important")
+        p.classList.toggle("important")
     })
-    titre.classList.add("important");
+    titre.classList.toggle("important");
 }
